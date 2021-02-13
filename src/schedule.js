1c351a993d3deff92b2710e15b3f3436277f174a
CharmingKyu <loadkrnis@gmail.com>
2021-02-14 00:20:46 +0900

    modify schedule.js

--- src/schedule.js
+++ src/schedule.js
@@ -2,6 +2,7 @@ const schedule = require('node-schedule');
 
 // var rule = new schedule.RecurrenceRule();
 // rule.second = 5;
+console.log("start");
 schedule.scheduleJob('3 * * * *', function(){
   console.log('매 3초에 실행');
 });
