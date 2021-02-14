639efbd06e4d446eb59f776f484b107815f9c3a0
CharmingKyu <loadkrnis@gmail.com>
2021-02-14 14:19:10 +0900

    modify index.js module getHtml

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const log = console.log;
 
-var maxPostNumber = 30;
+var maxPostNumber = 28;
 
 let getHtml = async () => {
   try {
-    console.log("try!");
     return await axios.get("https://www.bc.ac.kr/user/nd70345.do");
   } catch (error) {
     console.error(error);
@@ -14,30 +13,31 @@ let getHtml = async () => {
     // return await getHtml.postList;
   }
 };
- 
-getHtml()
-  .then(html => {
-    console.log("then1");
-    let postList = [];
-    const $ = cheerio.load(html.data);
-    const $postNumber = $("tbody").children("tr");
+setInterval(function () {
+  getHtml()
+    .then(html => {
+      let postList = [];
+      const $ = cheerio.load(html.data);
+      const $postNumber = $("tbody").children("tr");
 
-    $postNumber.each(function(i, elem) {
-      postList[i] = {
-        postNumber: $(this).find('td:nth-of-type(1)').text().trim(),
-        content: $(this).find('td:nth-of-type(2)').text().trim()
-      };
-      if(maxPostNumber == parseInt(postList[i].postNumber) + 1) {
-        maxPostNumber = parseInt(postList[i].postNumber) + 1;
-        console.log("새로운 게시물 발생\n게시물번호 : " + postList[i].postNumber + "\n게시물 : " + postList[i].content);
-      }
-    });
+      $postNumber.each(function (i, elem) {
+        postList[i] = {
+          postNumber: $(this).find('td:nth-of-type(1)').text().trim(),
+          content: $(this).find('td:nth-of-type(2)').text().trim()
+        };
+        if (maxPostNumber + 1 == parseInt(postList[i].postNumber)) {
+          maxPostNumber++;
+          console.log("새로운 게시물 발생\n게시물번호 : " + postList[i].postNumber + "\n게시물 : " + postList[i].content);
+        }
+      });
 
-    // const data = ulList.filter(n => n.title);
-    return postList;
-  })
-  .then(res => log("현재 최대 게시물 번호 : " + maxPostNumber));
+      // const data = ulList.filter(n => n.title);
+      return postList;
+    })
+    .then(res => log("현재 최대 게시물 번호 : " + maxPostNumber));
 
-  module.exports = {
-    getHtml
-  }
+}, 10000000);
+
+module.exports = {
+  getHtml
+}
