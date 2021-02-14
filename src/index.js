74983910a28e5a588ec7492b1d5a356782fb4792
CharmingKyu <loadkrnis@gmail.com>
2021-02-14 14:08:30 +0900

    modify getHtml module

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const log = console.log;
 
-const getHtml = async () => {
+var maxPostNumber = 30;
+
+let getHtml = async () => {
   try {
+    console.log("try!");
     return await axios.get("https://www.bc.ac.kr/user/nd70345.do");
   } catch (error) {
     console.error(error);
+  } finally {
+    // return await getHtml.postList;
   }
 };
  
 getHtml()
   .then(html => {
+    console.log("then1");
     let postList = [];
     const $ = cheerio.load(html.data);
     const $postNumber = $("tbody").children("tr");
@@ -21,9 +27,17 @@ getHtml()
         postNumber: $(this).find('td:nth-of-type(1)').text().trim(),
         content: $(this).find('td:nth-of-type(2)').text().trim()
       };
+      if(maxPostNumber == parseInt(postList[i].postNumber) + 1) {
+        maxPostNumber = parseInt(postList[i].postNumber) + 1;
+        console.log("새로운 게시물 발생\n게시물번호 : " + postList[i].postNumber + "\n게시물 : " + postList[i].content);
+      }
     });
 
     // const data = ulList.filter(n => n.title);
     return postList;
   })
-  .then(res => log(res));
+  .then(res => log("현재 최대 게시물 번호 : " + maxPostNumber));
+
+  module.exports = {
+    getHtml
+  }
--- src/schedule.js
+++ src/schedule.js
@@ -1,8 +1,7 @@
-const schedule = require('node-schedule');
-
-// var rule = new schedule.RecurrenceRule();
-// rule.second = 5;
-console.log("start");
-schedule.scheduleJob('3 * * * *', function(){
-  console.log('매 3초에 실행');
-});
+const posts = require('./index');
+ 
+console.log("start!");
+setInterval(function() {
+  console.log("tick!");
+  posts.getHtml()
+}, 3000);
